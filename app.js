6481f7e7e005b5cfdf2932afee80737714d3a2d3
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2025-08-26 14:16:03 +0100

    version 3.0
    Done with the icon switching and appearance
    Next will be on Responsiveness

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const card = document.querySelectorAll(".card");
+const minus = document.querySelectorAll(".minus");
+const plus = document.querySelectorAll(".plus");
 const button = document.querySelectorAll("button");
 const paragraph = document.querySelectorAll(".para");
 
 button.forEach((btn, index) => {
-
   btn.addEventListener("click", (e) => {
-
     const tab = e.target;
+
     if (["BUTTON", "STRONG", "IMG"].includes(tab.tagName)) {
       paragraph[index].classList.toggle("active");
+      plus[index].classList.toggle("active");
+      minus[index].classList.toggle("active");
     }
   });
 });
